9db6e7790056f3a59456a63dcc8ded423dccd70c
szymonguziec92-create <szymon.guziec92@gmail.com>
2026-08-19 14:04:51 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect, useRef, useMemo, useCallback } from "react";
+import { getDailySnack, getDietSummary } from "./snack-utils.js";
 import { getWaterAmount, changeWater } from "./water-utils.js";
 import TrainingGeneratorTabNew from "./TrainingGeneratorTabNew.jsx";
 import { getStoredSession, restoreSession, signInWithPassword, signUpWithPassword, signOutCloud, loadCloudData, saveCloudData } from "./cloud-sync.js";
